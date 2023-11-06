860f6c8fa285cab11f9b3001200bcab79467582f
MaxiTRR <maxihtorr@gmail.com>
2023-11-06 19:34:44 -0300

    add option 1 to menu_providers, minor fix in menu_principal

--- menu_provider_ferreteria.py
+++ menu_provider_ferreteria.py
@@ -10,3 +10,14 @@ def menu_providers():
         #REGISTRAR/ALTA PROVEEDOR
         while opc_Chosen == "1":
             dni_prov = inputDni("Ingrese el dni del proveedor que deser registrar o dar de alta: ")
+            result = Provider.query_dni_and_alta_prov(dni_prov)
+            if not result:
+                print("El proveedor no se encuentra registrado. Puede ingresar sus datos.")
+            else:
+                if result[1] == 1:
+                    print("El proveedor ya se encuentra dado de alta.")
+                else:
+                    print("El proveedor se encuentra dado de baja, por lo tanto se dara de Alta.")
+
+
+
