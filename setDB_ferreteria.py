ebf6c313f3d7d341abd88a9e987a0255d602f87f
MaxiTRR <maxihtorr@gmail.com>
2023-11-06 15:45:45 -0300

    add set_Art_reg to to setDB_ferreteria

--- setDB_ferreteria.py
+++ setDB_ferreteria.py
@@ -1,6 +1,7 @@
 from ferreteriaDB import *
 from ferreteria_Client import *
 from ferreteria_Prov import *
+from ferreteria_Art import *
 
 #Metodos para setear la base de datos con registros iniciales. COMENTAR Y DESCOMENTAR.
 BD = "ferreteria"
@@ -23,4 +24,9 @@ def setClientRegs():
 def setProvRegs():
     Provider.reg_Prov(69542178, "Black & Decker", "Ramon Falcon 345, Flores, CABA", 42896524, "black@decker.com", "inscripto", True)
 
-setProvRegs()
+#setProvRegs()
+
+def setArtReg():
+    Article.reg_Art(3100, "Pinza De Punta","herramienta manual", 7.791, 0, 42896524, True)
+
+setArtReg()
