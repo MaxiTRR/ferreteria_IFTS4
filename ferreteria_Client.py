8fd0086cd0a3b3e6d145ab7e8b2f20dbe94c303b
MaxiTRR <maxihtorr@gmail.com>
2023-11-13 20:44:10 -0300

    change clients table and add register client option

--- ferreteria_Client.py
+++ ferreteria_Client.py
@@ -16,10 +16,20 @@ class Client():
         pass
 
     @classmethod
-    def reg_Client(self, dni_cli, nombre_cli, apellido_cli, dir_cli, tel_cli, mail_cli, sit_IVA_cli, alta_cli = True):
+    def reg_Client(self, dni_cli, nombre_cli, apellido_cli, dir_cli, tel_cli, mail_cli, sit_IVA_cli, alta_cli = True, obs_cli = ""):
         mydb = self.Database
         mycursor = mydb.cursor()
-        sql = "INSERT INTO clientes (dni_cli, nombre_cli, apellido_cli, dir_cli, tel_cli, mail_cli, sit_IVA_cli, alta_cli) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-        val = (dni_cli, nombre_cli, apellido_cli, dir_cli, tel_cli, mail_cli, sit_IVA_cli, alta_cli)
+        sql = "INSERT INTO clientes (dni_cli, nombre_cli, apellido_cli, dir_cli, tel_cli, mail_cli, sit_IVA_cli, alta_cli, obs_cli) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        val = (dni_cli, nombre_cli, apellido_cli, dir_cli, tel_cli, mail_cli, sit_IVA_cli, alta_cli, obs_cli)
         mycursor.execute(sql, val)
         mydb.commit()
+
+    @classmethod
+    def query_client(self, dni_cli):
+        mydb = self.Database
+        mycursor = mydb.cursor()
+        sql = f"SELECT * FROM clientes WHERE dni_cli = {dni_cli}"
+        mycursor.execute(sql)
+        myresultado = mycursor.fetchone()
+        print(myresultado)  # REVISAR ESTE PRINT
+        return myresultado
